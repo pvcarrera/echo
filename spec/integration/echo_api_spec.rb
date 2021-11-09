054c2e52962c3df495d5c9524538ad3d55b7cdd6
Pablo Vicente <vicapa99@gmail.com>
2021-11-09 20:08:12 +0100

    Use let instead of Ruby methods
    This is mainly to keep consistency over the spec files

--- spec/integration/echo_api_spec.rb
+++ spec/integration/echo_api_spec.rb
@@ -8,27 +8,22 @@ RSpec.describe EchoAPI do
   include Rack::Test::Methods
   include Committee::Test::Methods
 
-  def app
+  let!(:app) do
     @app ||= Rack::Builder.parse_file('config.ru').first
   end
+  let!(:endpoint_repository) { EndpointsRepository.new }
 
-  def committee_options
+  let!(:committee_options) do
     api_definition_path = File.join(
       File.dirname(__FILE__),
       '../../openapi_spec/echo_api.yml'
     )
     @committee_options ||= { schema: Committee::Drivers.load_from_file(api_definition_path) }
   end
-
   # Committee::Test needs this method
-  def request_object
-    last_request
-  end
-
+  let(:request_object) { last_request }
   # Committee::Test needs this method
-  def response_data
-    [last_response.status, last_response.headers, last_response.body]
-  end
+  let(:response_data) { [last_response.status, last_response.headers, last_response.body] }
 
   describe 'a GET request to the endpoints resource' do
     it 'returns an empty list' do
